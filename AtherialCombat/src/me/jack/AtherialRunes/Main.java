4202487c9b3ad1de797b3bcf876aa70dc8432363
Qroundhawk <t0mmy4@hotmail.com>
2016-08-13 21:37:27 +0200

    Test int

--- AtherialCombat/src/me/jack/AtherialRunes/Main.java
+++ AtherialCombat/src/me/jack/AtherialRunes/Main.java
@@ -4,22 +4,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import net.md_5.bungee.api.ChatColor;
 
-public class Main extends JavaPlugin {
+public class Main extends JavaPlugin 
+{
 	
 	
 	
 	
-	public void onEnable() {
+	public void onEnable() 
+	{
 		System.out.print(ChatColor.GREEN + "Etherial Combat is working!");
 		
 	
 	}
 	
 	
-	public void onDisable() {
+	public void onDisable() 
+	{
 		System.out.print(ChatColor.RED + "Etherial Combat disabled.");
 	}
 	
+	private int test = 1;
 	
 
 }
