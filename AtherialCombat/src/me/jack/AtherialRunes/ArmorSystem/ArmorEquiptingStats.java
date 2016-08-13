d4d11c4fb4be2f87b46de229c33feffcb0776b66
Jack <jaxon29@Outlook.com>
2016-08-13 15:00:49 -0500

    sdas

--- AtherialCombat/src/me/jack/AtherialRunes/ArmorSystem/ArmorEquiptingStats.java
+++ AtherialCombat/src/me/jack/AtherialRunes/ArmorSystem/ArmorEquiptingStats.java
@@ -1,5 +1,23 @@
 package me.jack.AtherialRunes.ArmorSystem;
 
-public class ArmorEquiptingStats {
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import me.jack.AtherialRunes.libs.ArmorEquipEvent.ArmorEquipEvent;
+
+public class ArmorEquiptingStats implements Listener {
+	
+	
+	
+	
+	
+	@EventHandler
+	public void onEquiptArmorArmor(ArmorEquipEvent e) {
+		ItemMeta HArmor = e.getPlayer().getInventory().getHelmet().getItemMeta();
+		if(HArmor.getLore().contains("ARMOR:")) {
+			int Harmora = Integer.parseInt(HArmor.getLore().get(0).replaceAll("[^0-9]",""));
+		}
+	}
 
 }
