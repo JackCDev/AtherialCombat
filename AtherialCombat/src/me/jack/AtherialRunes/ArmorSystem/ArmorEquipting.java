33077bbe051daada6217634059b6745dc093f50e
Qroundhawk <t0mmy4@hotmail.com>
2016-08-13 22:15:29 +0200

    Added equiping armor health

--- AtherialCombat/src/me/jack/AtherialRunes/ArmorSystem/ArmorEquipting.java
+++ AtherialCombat/src/me/jack/AtherialRunes/ArmorSystem/ArmorEquipting.java
@@ -1,5 +1,66 @@
 package me.jack.AtherialRunes.ArmorSystem;
 
-public class ArmorEquipting {
+import java.util.Scanner;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import me.jack.AtherialRunes.libs.ArmorEquipEvent.ArmorEquipEvent;
+
+public class ArmorEquipting implements Listener 
+{
+	@EventHandler
+	public void Equip(ArmorEquipEvent e) 
+	{
+
+		Player p = e.getPlayer();
+		
+		//Checking if inventory is full
+		if (p.getInventory().firstEmpty() == -1) 
+		{
+			e.setCancelled(true);
+			p.sendMessage(ChatColor.RED + "Inventory Full!");
+			return;
+		}
+		
+		e.getType().getSlot();
+		
+		if (e.getNewArmorPiece() != null && e.getNewArmorPiece().hasItemMeta()) 
+		{
+			// ----------Setting Health----------
+			String lorehealth = e.getNewArmorPiece().getItemMeta().getLore().get(2);
+			Scanner in2 = new Scanner(lorehealth).useDelimiter("[^0-9]+");
+			int h = in2.nextInt();
+			double maxhealth = p.getMaxHealth();
+			p.setMaxHealth(maxhealth + h);
+			
+			
+			p.sendMessage("");
+			p.sendMessage(ChatColor.DARK_PURPLE + "    ■You equipped: "
+					+ e.getNewArmorPiece().getItemMeta().getDisplayName());
+			p.sendMessage(ChatColor.DARK_PURPLE + "    ■" + ChatColor.WHITE + "MaxHealth is now: "
+					+ ChatColor.DARK_GREEN + ChatColor.BOLD.toString() + p.getMaxHealth() + "Health");
+			p.sendMessage("");
+		}
+
+		if (e.getOldArmorPiece() != null && e.getOldArmorPiece().hasItemMeta()) {
+			// ----------Setting Health----------
+			String lorehealth = e.getOldArmorPiece().getItemMeta().getLore().get(2);
+			Scanner in2 = new Scanner(lorehealth).useDelimiter("[^0-9]+");
+			int h = in2.nextInt();
+			double maxhealth = p.getMaxHealth();
+			p.setMaxHealth(maxhealth - h);
+
+
+			p.sendMessage("");
+			p.sendMessage(ChatColor.DARK_PURPLE + "    ■You unequipped: "
+					+ e.getOldArmorPiece().getItemMeta().getDisplayName());
+			p.sendMessage(ChatColor.DARK_PURPLE + "    ■" + ChatColor.WHITE + "MaxHealth is now: "
+					+ ChatColor.DARK_GREEN + ChatColor.BOLD.toString() + p.getMaxHealth() + "Health");
+			p.sendMessage("");
+		}
+	}
 
 }
