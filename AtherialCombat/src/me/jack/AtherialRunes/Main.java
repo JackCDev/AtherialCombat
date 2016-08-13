c51629d6a0fda1d896a000e7cf86c2530c9251e0
Jack <jaxon29@Outlook.com>
2016-08-13 14:13:57 -0500

    Fixes

--- AtherialCombat/src/me/jack/AtherialRunes/Main.java
+++ AtherialCombat/src/me/jack/AtherialRunes/Main.java
@@ -1,5 +1,25 @@
 package me.jack.AtherialRunes;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Main extends JavaPlugin {
+	
+	
+	
+	
+	public void onEnable() {
+		System.out.print(ChatColor.GREEN + "Etherial Combat is working!");
+		
+	
+	}
+	
+	
+	public void onDisable() {
+		System.out.print(ChatColor.RED + "Etherial Combat disabled.");
+	}
+	
+	
 
 }
